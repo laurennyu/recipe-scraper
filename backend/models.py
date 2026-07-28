2760a8884fc38ba571c8f4ac50bf54600fd410a7
laurennyu <ly39@rice.edu>
2026-07-28 14:35:56 -0400

    Added support for ingredient parsing with NLP

--- backend/models.py
+++ backend/models.py
@@ -4,14 +4,14 @@ class RecipeRequest(BaseModel):
     url: str
     html: str
 
-# class Ingredient(BaseModel):
-#     quantity: float | None = None
-#     unit: str | None = None
-#     name: str
+class Ingredient(BaseModel):
+    quantity: str | None = None
+    unit: str | None = None
+    name: str
 
 class Recipe(BaseModel):
     title: str
-    ingredients: list[str] #list[Ingredient]
+    ingredients: list[Ingredient]
     instructions: list[str]
     total_time: int | None = None
     yields: str | None = None
--- backend/parser.py
+++ backend/parser.py
@@ -1,17 +1,37 @@
 from recipe_scrapers import scrape_html
+from ingredient_parser import parse_ingredient
 
-from models import Recipe, RecipeRequest
+from models import Recipe, RecipeRequest, Ingredient
+
+def parse_ingredients(ingredients_list: list[str]) -> list[Ingredient]:
+    print("Parsing ingredients list for amounts/units/item names")
+    # Parse each ingredient in the list
+    parsed_ingredients = [parse_ingredient(ingredient) for ingredient in ingredients_list]
+    print(parsed_ingredients)
+
+    parsed_ingredients = [Ingredient(quantity=str(ingredient.amount[0].quantity) if ingredient.amount else None,
+                                     unit=str(ingredient.amount[0].unit) if ingredient.amount else None,
+                                     name=ingredient.name[0].text)
+                                     for ingredient in parsed_ingredients]
+    # TODO: Add error handling
+
+    print("Parsed ingredients:", parsed_ingredients)
+    return parsed_ingredients
 
 def parse_recipe(request: RecipeRequest):
-
+    # Scrape the recipe from the HTML content
     scraper = scrape_html(
         request.html,
         org_url=request.url
     )
 
+    # Parse the ingredients
+    ingredients_list = parse_ingredients(scraper.ingredients())
+
+    # Return a Recipe object with the scraped data
     return Recipe(
         title=scraper.title(),
-        ingredients=scraper.ingredients(),
+        ingredients=ingredients_list,
         instructions=scraper.instructions_list(),
         total_time=scraper.total_time(),
         yields=scraper.yields(),
