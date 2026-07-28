014d23f8a9d9759741262f2a58a31e5629577879
laurennyu <ly39@rice.edu>
2026-07-28 14:03:03 -0400

    Add middleware for extension support

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import RecipeRequest
 from parser import parse_recipe
 from storage import save_recipe
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # Fine for local development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/save")
 def save(request: RecipeRequest):
